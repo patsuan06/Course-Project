4f367435211df45d949a91154bcfb9eb92cc9313
patsuan06 <irrll949@gmail.com>
2025-10-18 02:50:39 +0600

    Initial commit

---
+++ src/main/java/com/trinity/crourierapp/CrourierAppApplication.java
@@ -0,0 +1,13 @@
+package com.trinity.crourierapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrourierAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrourierAppApplication.class, args);
+    }
+
+}
