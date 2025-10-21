2e2de5fb4f5f3d819a6fbabfa1dc07ec192b0a93
sanchezz <checking99@mail.ru>
2025-10-21 20:59:25 +0600

    test commit

--- src/main/java/com/trinity/crourierapp/CrourierAppApplication.java
+++ src/main/java/com/trinity/crourierapp/CrourierAppApplication.java
@@ -2,9 +2,10 @@ package com.trinity.crourierapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
-public class CrourierAppApplication {
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})public class CrourierAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CrourierAppApplication.class, args);
---
+++ src/main/java/com/trinity/crourierapp/HelloController.java
@@ -0,0 +1,13 @@
+package com.trinity.crourierapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String helloController(){
+        return "Wassup!";
+    }
+}
