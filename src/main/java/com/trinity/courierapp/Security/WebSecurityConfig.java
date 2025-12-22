7c1a0e770c3353a80c4ce959d033e539d4862d2d
patsuan06 <irrll949@gmail.com>
2025-12-22 22:04:11 +0600

    test

--- src/main/java/com/trinity/courierapp/Security/WebSecurityConfig.java
+++ src/main/java/com/trinity/courierapp/Security/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -53,7 +54,8 @@ public class WebSecurityConfig {
         http
 //                .cors(AbstractHttpConfigurer::disable)
                 .csrf(csrf -> csrf.disable())
-                .cors(cors -> cors.disable())
+//                .cors(cors -> cors.disable())
+                .cors((Customizer.withDefaults()))
                 .exceptionHandling(e ->
                         e.authenticationEntryPoint(unauthorizedHandler)
                 )
